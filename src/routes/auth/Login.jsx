8ecc2e5e0aa8c5b1c381a17e7b46777c43d6b58a
Divine <divinetidy@gmail.com>
2025-05-17 21:37:08 +0100

    working on auth ui

--- src/routes/auth/Login.jsx
+++ src/routes/auth/Login.jsx
@@ -1,39 +1,59 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const { register, handleSubmit, watch } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  const email = watch("email");
+  const password = watch("password");
   return (
-    <form className="text-black h-screen bg-[#F5F5F5] flex flex-col justify-center items-center ">
-      <div className="flex flex-col w-[600px] py-5 px-10 bg-[#FDFDFD] border-[1px] border-[#F2F2F2] rounded-[16px]">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="text-black h-screen bg-[#F5F5F5] px-3 flex flex-col justify-center items-center "
+    >
+      <div className="flex flex-col w-full px-3 md:w-[600px] py-5 md:px-10 bg-[#FDFDFD] border-[1px] border-[#F2F2F2] rounded-[16px]">
         <div className="w-full flex justify-center">
-          <img src="\image\payNestLogo.svg" alt="logo" className="w-[85px] " />
+          <img
+            src="\image\payNestLogo.svg"
+            alt="logo"
+            className="md:w-[85px] w-[60px]"
+          />
         </div>
         <div className="mt-7">
-          <h1 className="font-inter font-bold text-[30px] text-black text-center ">
+          <h1 className="font-inter font-bold text-2xl md:text-[30px] text-black text-center ">
             Welcome Back!
           </h1>
           <div className="space-y-[30px] mt-5 text-base">
-            <div className="flex flex-col">
+            <div className="flex flex-col  gap-2">
               <label>Email Address</label>
               <input
+                {...register("email", { required: true })}
                 type="text"
                 placeholder="abc@gmail.com"
-                className="border-[1px] text-[#2B2B2B] border-[#DBDBDB] rounded-[8px] outline-0 py-3.5 px-4"
+                className={`duration-300 ease-in-out border-[1px] text-[#2B2B2B] rounded-[8px] outline-0 py-3.5 px-4 ${
+                  email ? "border-[#474ED3]" : "border-[#DBDBDB]"
+                }`}
               />
             </div>
-            <div className="flex flex-col">
+            <div className="flex flex-col  gap-2">
               <label>Password</label>
               <input
+                {...register("password", { required: true })}
                 type="text"
                 placeholder="xxxxxxxxxxxxx"
-                className="border-[1px] text-[#2B2B2B] border-[#DBDBDB] rounded-[8px] outline-0 py-3.5 px-4"
+                className={`duration-300 ease-in-out border-[1px] text-[#2B2B2B] rounded-[8px] outline-0 py-3.5 px-4 ${
+                  password ? "border-[#474ED3]" : "border-[#DBDBDB]"
+                }`}
               />
-              <Link className="text-end text-style-underline">Forgot password?</Link>
+              <Link className="text-end underline ">Forgot password?</Link>
             </div>
           </div>
         </div>
         <button className="bg-[#474ED3] text-[#F0F1FF] mt-10 py-4 rounded-[8px] w-full font-inter text-base font-normal cursor-pointer">
-          Create Account
+          Login
         </button>
         <span className="text-base font-normal text-center mt-10">
           Don’t have an account?
--- src/routes/auth/Register.jsx
+++ src/routes/auth/Register.jsx
@@ -10,8 +10,13 @@ const Register = () => {
     register,
     formState: { errors },
     handleSubmit,
+    watch,
   } = useForm();
   const [registerData, setRegisterData] = useState();
+  const firstName = watch("first_Name");
+  const lastName = watch("last_Name");
+  const email = watch("email");
+  const password = watch("password");
 
   const handleChange = (index) => (e) => {
     const val = e.target.value.replace(/\D/g, ""); // Remove non-digits
@@ -32,133 +37,145 @@ const Register = () => {
   };
 
   return (
-    <div className="bg-[#F5F5F5] px-7 lg:px-0 h-screen w-full flex justify-center items-center">
-        {/* Registration Form */}
-        <div
-          className={`${
-            activeCodeField ? "flex" : "hidden"
-          } flex-col py-5 px-5 md:px-10 bg-[#FDFDFD] border-[1px] border-[#F2F2F2] rounded-[16px] w-full md:w-[600px]`}
-        >
-          <div className="w-full flex justify-center">
-            <img src="/image/payNestLogo.svg" alt="logo" className="w-[85px]" />
-          </div>
-          <div className="flex flex-col justify-center mt-7">
-            <h1 className="font-inter font-bold text-[30px] text-black text-center">
-              Welcome to PayNest
-            </h1>
-            <form
-              onSubmit={handleSubmit(onSubmit)}
-              className="w-full mt-5 space-y-[30px] font-inter font-normal text-base"
-            >
-              <div className="text-[#2B2B2B] w-full  md:flex items-center justify-between">
-                <div className="flex flex-col lg:w-[200px]">
-                  <label>First Name</label>
-                  <input
-                    type="text"
-                    {...register("first_Name", { required: true })}
-                    aria-invalid={errors.first_Name ? "true" : "false"}
-                    placeholder="Divine"
-                    className="border-[1px] text-[#2B2B2B] border-[#DBDBDB] rounded-[8px] outline-0 py-3.5 px-4"
-                  />
-                  {errors.first_Name?.type === "required" && (
-                    <p role="alert" className="text-sm text-red-400">
-                      *
-                    </p>
-                  )}
-                </div>
-                <div className="flex flex-col lg:w-[200px]">
-                  <label>Last Name</label>
-                  <input
-                    type="text"
-                    {...register("last_Name", { required: true })}
-                    aria-invalid={errors.last_Name ? "true" : "false"}
-                    placeholder="Tiddy"
-                    className="border-[1px] text-[#2B2B2B] border-[#DBDBDB] rounded-[8px] outline-0 py-3.5 px-4"
-                  />
-                  {errors.last_Name?.type === "required" && (
-                    <p role="alert" className="text-sm text-red-400">
-                      *
-                    </p>
-                  )}
-                </div>
-              </div>
-              <div className="flex flex-col">
-                <label>Email Address</label>
-                <input
-                  type="email"
-                  {...register("email", { required: true })}
-                  aria-invalid={errors.email ? "true" : "false"}
-                  placeholder="example@gmail.com"
-                  className="border-[1px] text-[#2B2B2B] border-[#DBDBDB] rounded-[8px] outline-0 py-3.5 px-4"
-                />
-                {errors.email?.type === "required" && (
-                  <p role="alert" className="text-sm text-red-400">
-                    *
-                  </p>
-                )}
-              </div>
-              <div className="flex flex-col">
-                <label>Create Password</label>
-                <input
-                  type="password"
-                  {...register("password", { required: true, maxLength: 20 })}
-                  aria-invalid={errors.password ? "true" : "false"}
-                  placeholder="********"
-                  className="border-[1px] text-[#2B2B2B] border-[#DBDBDB] rounded-[8px] outline-0 py-3.5 px-4"
-                />
-                {errors.password?.type === "required" && (
-                  <p role="alert" className="text-sm text-red-400">
-                    *
-                  </p>
-                )}
-              </div>
-              <button
-                type="submit"
-                className="bg-[#474ED3] text-[#F0F1FF] py-4 rounded-[8px] w-full text-center text-base font-normal cursor-pointer"
-              >
-                Create Account
-              </button>
-            </form>
-            <span className="mt-3 text-center">
-              Have an account?
-              <Link to="/login" className="font-semibold ml-1">
-                Login
-              </Link>
-            </span>
-          </div>
+    <div className="bg-[#F5F5F5] px-3   lg:px-10 h-screen w-full flex justify-center items-center">
+      {/* Registration Form */}
+      <div
+        className={`${
+          activeCodeField ? "flex" : "hidden"
+        } flex-col py-5 px-5 md:px-10 bg-[#FDFDFD]  border-[1px] border-[#F2F2F2] rounded-[16px] w-full md:w-[600px]`}
+      >
+        <div className="w-full flex justify-center">
+          <img
+            src="/image/payNestLogo.svg"
+            alt="logo"
+            className="w-[60px] md:w-[85px]"
+          />
         </div>
-
-        {/* Email Confirmation */}
-        <form
-          onSubmit={codeSubmit}
-          className={`${
-            activeCodeField ? "hidden" : "flex"
-          } font-inter flex-col items-center py-5 px-10 bg-[#FDFDFD] border-[1px] border-[#F2F2F2] rounded-[16px]`}
-        >
-          <h1 className="font-inter font-bold text-2xl text-black text-center">
-            Verify Your Email
+        <div className="flex flex-col justify-center mt-7">
+          <h1 className="font-inter font-bold text-2xl md:text-[30px] text-black text-center">
+            Welcome to PayNest
           </h1>
-          <p className="text-[#767676] font-normal text-base mt-3">
-            Enter the six(6) digit code sent to abcxxx@gmail.com
-          </p>
-          <div className="flex gap-4 mt-5">
-            {Array.from({ length: 6 }).map((_, index) => (
-              <CodeInput
-                key={index}
-                value={code[index] || ""}
-                onChange={handleChange(index)}
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="w-full mt-5 space-y-[30px] font-inter font-normal text-base"
+          >
+            <div className="text-[#2B2B2B] w-full space-y-4 md:space-y-0  md:flex items-center justify-between">
+              <div className="  flex flex-col gap-2 lg:w-[200px]">
+                <label>First Name</label>
+                <input
+                  type="text"
+                  {...register("first_Name", { required: true })}
+                  aria-invalid={errors.first_Name ? "true" : "false"}
+                  placeholder="Divine"
+                  className={`duration-300 ease-in-out border-[1px] text-[#2B2B2B] rounded-[8px] outline-0 py-3.5 px-4 ${
+                    firstName ? "border-[#474ED3]" : "border-[#DBDBDB]"
+                  }`}
+                />
+                {errors.first_Name?.type === "required" && (
+                  <p role="alert" className="text-sm text-red-400">
+                    *
+                  </p>
+                )}
+              </div>
+              <div className="flex flex-col gap-2 lg:w-[200px]">
+                <label>Last Name</label>
+                <input
+                  type="text"
+                  {...register("last_Name", { required: true })}
+                  aria-invalid={errors.last_Name ? "true" : "false"}
+                  placeholder="Tiddy"
+                  className={`duration-300 ease-in-out border-[1px] text-[#2B2B2B] rounded-[8px] outline-0 py-3.5 px-4 ${
+                    lastName ? "border-[#474ED3]" : "border-[#DBDBDB]"
+                  }`}
+                />
+                {errors.last_Name?.type === "required" && (
+                  <p role="alert" className="text-sm text-red-400">
+                    *
+                  </p>
+                )}
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              <label>Email Address</label>
+              <input
+                type="email"
+                {...register("email", { required: true })}
+                aria-invalid={errors.email ? "true" : "false"}
+                placeholder="example@gmail.com"
+                className={`duration-300 ease-in-out border-[1px] text-[#2B2B2B] rounded-[8px] outline-0 py-3.5 px-4 ${
+                  email ? "border-[#474ED3]" : "border-[#DBDBDB]"
+                }`}
               />
-            ))}
-          </div>
-          <span className="font-normal text-base text-center mt-5">
-            Didn’t receive code?
-            <Link className="font-semibold ml-2">Resend.</Link>
+              {errors.email?.type === "required" && (
+                <p role="alert" className="text-sm text-red-400">
+                  *
+                </p>
+              )}
+            </div>
+            <div className="flex flex-col gap-2">
+              <label>Create Password</label>
+              <input
+                type="password"
+                {...register("password", { required: true, minLength: 20 })}
+                aria-invalid={errors.password ? "true" : "false"}
+                placeholder="********"
+                className={`duration-300 ease-in-out border-[1px] text-[#2B2B2B] rounded-[8px] outline-0 py-3.5 px-4 ${
+                  password ? "border-[#474ED3] " : "border-[#DBDBDB]"
+                }`}
+              />
+              {errors.password?.type === "required" && (
+                <p role="alert" className="text-sm text-red-400">
+                  *
+                </p>
+              )}
+            </div>
+            <button
+              type="submit"
+              className="bg-[#474ED3] text-[#F0F1FF] py-4 rounded-[8px] w-full text-center text-base font-normal cursor-pointer"
+            >
+              Create Account
+            </button>
+          </form>
+          <span className="mt-10 md:mt-3 text-center">
+            Have an account?
+            <Link to="/login" className="font-semibold ml-1">
+              Login
+            </Link>
           </span>
-          <button className="bg-[#474ED3] text-[#F0F1FF] mt-5 py-4 rounded-[8px] w-full text-base font-normal cursor-pointer">
-            Verify & Continue
-          </button>
-        </form>
+        </div>
       </div>
+
+      {/* Email Confirmation */}
+      <form
+        onSubmit={codeSubmit}
+        className={`${
+          activeCodeField ? "hidden" : "flex"
+        } font-inter flex-col items-center py-5 px-10 bg-[#FDFDFD] border-[1px] border-[#F2F2F2] rounded-[16px]`}
+      >
+        <h1 className="font-inter font-bold text-2xl text-black text-center">
+          Verify Your Email
+        </h1>
+        <p className="text-[#767676] font-normal text-base mt-3 text-center">
+          Enter the six(6) digit code sent to abcxxx@gmail.com
+        </p>
+        <div className="flex gap-4 mt-5">
+          {Array.from({ length: 6 }).map((_, index) => (
+            <CodeInput
+              key={index}
+              value={code[index] || ""}
+              onChange={handleChange(index)}
+            />
+          ))}
+        </div>
+        <span className="font-normal text-base text-center mt-5">
+          Didn’t receive code?
+          <Link className="font-semibold ml-2">Resend.</Link>
+        </span>
+        <button className="bg-[#474ED3] text-[#F0F1FF] mt-5 py-4 rounded-[8px] w-full text-base font-normal cursor-pointer">
+          Verify & Continue
+        </button>
+      </form>
+    </div>
   );
 };
 
