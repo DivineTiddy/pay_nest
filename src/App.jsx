986d53473caac2d819da6869b765407cae6feba3
Divine <divinetidy@gmail.com>
2025-05-28 09:05:51 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
           <Route path="/verify" element={<Verify />} />
           <Route path="/register" element={<Register />} />
           {/* <Route path="/login" element={<Login />} /> */}
-          <Route path="/login" element={<DashBoard />} />
+          <Route path="/dashboard" element={<DashBoard />} />
           <Route path="/send" element={<Send />} />
           <Route path="/details" element={<TransferDetails />} />
           <Route path="/success" element={<Success />} />
